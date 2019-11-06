57e02c7aade72aaaebd786545cd41895aa374ab6
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-11-07 00:47:59 +0100

    nightModeCallback, endCallback and progressCallback done

--- src/components/containers/WbnPlayer.js
+++ src/components/containers/WbnPlayer.js
@@ -58,15 +58,39 @@ const WbnPlayer = ({ match, history, location }) => {
     }, [history, location.autoplay, match.params.activeVideo, state.activeVideo, state.activeVideo.id, state.videos]);
 
     const nightModeCallback = () => {
-
+        setState(prev => ({
+            ...prev, nightMode: !prev.nightMode
+        }));
     }
 
     const endCallback = () => {
+        const videoId = match.params.activeVideo;
+        const currentVideoIndex = state.videos.findIndex(
+            video => video.id === videoId
+        );
 
+        const nextVideo = currentVideoIndex === state.videos.length - 1 ? 0 : currentVideoIndex + 1;
+        history.push({ pathname: `/${state.videos[nextVideo].id}`, autoplay: false});
     }
 
-    const progressCallback = () => {
+    const progressCallback = event => {
+        if(event.playedSeconds > 10 && event.playedSeconds < 11) {
+            const videos = [ ...state.videos ];
+            const playedVideo = videos.find(
+                video => video.id === state.activeVideo.id
+            )
 
+            playedVideo.played = true;
+
+            setState(prev => ({ ...prev, videos }));
+
+            // setState({
+            //     ...state,
+            //     videos: state.videos.map(video => {
+            //         return video.id === state.activeVideo.id ? { ...video, played: true} : video
+            //     })
+            // })
+        }
     }
 
     return (
